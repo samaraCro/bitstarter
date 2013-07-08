dee7e83a83c6eeb5c10009a3b42212a453ffb8d3
Igor Samardzija <igor.samardzija@infotehna.com>
2013-07-08 13:45:38 +0000

    modify web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var fs = require('fs');
+var readFile = fs.readFileSync('index.html');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+//  response.send('Hello World2!');
+	response.send(readFile.toString());
 });
 
 var port = process.env.PORT || 5000;
